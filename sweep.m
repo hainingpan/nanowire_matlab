ba8d895c71107b495754a705fb9e35e5692f981a
jakepan1994 <jack_pan1994@126.com>
2018-08-28 14:43:42 -0400

    parallel

--- sweep.m
+++ sweep.m
@@ -1,6 +1,7 @@
 mulist=0:.1:2;
 store=zeros(length(mulist),50);
-for i=1:length(mulist)
+parfor i=1:length(mulist)
     mu=mulist(i);
     store(i,:)=spec(mu,.2,2);
 end
+save('store.dat','store','-ascii');
