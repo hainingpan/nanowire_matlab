15c0ac3a1729202adcfe85f148be7f33ec0266ec
Haining Pan <jack_pan1994@126.com>
2018-08-31 15:27:23 -0400

    .

--- hmb.m
+++ hmb.m
@@ -1,4 +1,4 @@
-function re=hmb(a,mu,delta11,vz,alpha_R,dim)
+function re=hmb(a,mu,delta11,vz,alpha_R,deltac,dim)
 t=25/a^2;
 sx=[0,1;1,0];
 sy=[0,-1i;1i,0];
@@ -8,9 +8,8 @@ band11sm=(spdiags([ones(dim,1) ones(dim,1)],[-1,1],dim,dim));
 band1m1sm=(spdiags([ones(dim,1) -ones(dim,1)],[-1,1],dim,dim));
 eyesm=speye(dim);
 epsilon=1;
-delta12=.2;
 H11=kron(sz,(kron(eye(2),-t*band11sm+(-mu+2*t)*eyesm)+kron(sy,1i*alpha*band1m1sm)))+kron(eye(2),kron(sz,vz*eyesm))+kron(sx,kron(eye(2),delta11*eyesm));
 H22=kron(sz,(kron(eye(2),-t*band11sm+(epsilon-mu+2*t)*eyesm)+kron(sy,1i*alpha*band1m1sm)))+kron(eye(2),kron(sz,vz*eyesm))+kron(sx,kron(eye(2),delta11*eyesm));
-H12=kron(sx,kron(eye(2),delta12*eyesm));
+H12=kron(sx,kron(eye(2),deltac*eyesm));
 re=[H11,H12;H12,H22];
 end
--- sweep.m
+++ sweep.m
@@ -1,9 +1,10 @@
-function sweep(delta)
+function sweep(deltac)
+delta=.2
 mulist=0:.05:2;
 store=zeros(length(mulist),50);
 parfor i=1:length(mulist)
     mu=mulist(i);
-    store(i,:)=spec(mu,delta,2);
+    store(i,:)=spec(mu,delta,2,deltac);
 end
 save(strcat('deltac0.2',num2str(delta),'.dat'),'store','-ascii');
 end
