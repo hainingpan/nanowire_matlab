38b1393e499b6d2920f1351bad527b3eecbafce6
Haining Pan <jack_pan1994@126.com>
2018-10-28 20:13:41 -0400

    hmu

---
+++ hmu.m
@@ -0,0 +1,22 @@
+function re=hmu(a,mu,delta,vz,alpha_R,dim,smoothpot,mumax,peakpos)
+t=25/a^2;
+sx=[0,1;1,0];
+sy=[0,-1i;1i,0];
+sz=[1,0;0,-1];
+alpha=alpha_R/(2*a);
+band11sm=(spdiags([ones(dim,1) ones(dim,1)],[-1,1],dim,dim));
+band1m1sm=(spdiags([ones(dim,1) -ones(dim,1)],[-1,1],dim,dim));
+eyesm=speye(dim);
+switch smoothpot
+    case 'sin2'
+        mulist=sin(range(dim)*2*pi/dim)*mumax+mu;
+    case 'cos'
+        mulist=cos(range(dim)*pi/dim)*mumax+mu;
+    case 'lorentz'
+        mulist=mumax*1.0/(((range(dim)-peakpos*dim))^2+.5)+mu;
+    case 'lorentzsigmoid'
+        mulist=(mumax*1.0/((range(dim)-peakpos*dim)^2+.5)+(4-mu)/2./(exp(-(range(dim)-0.5*dim))+1))+mu;
+end      
+re=kron(sz,(kron(eye(2),-t*band11sm+(2*t)*eyesm-diag(mulist))+kron(sy,1i*alpha*band1m1sm)))+kron(eye(2),kron(sz,vz*eyesm))+kron(sx,kron(eye(2),delta*eyesm));
+end
+
---
+++ spec0.m
@@ -0,0 +1,35 @@
+%%for single band 
+function [rev,re]=spec0(mu,dim,smoothpot,mumax,peakpos)
+a=1;
+delta=0.2;
+alpha=5;
+vzlist=0:0.1:4;
+nv=20;
+en=zeros(nv,length(vzlist));
+parfor i=1:length(vzlist)
+    vz=vzlist(i);
+    ham=hmu(a,mu,delta,vz,alpha,dim,smoothpot,mumax,peakpos);
+    eigo=eigs(ham,nv,0,'Tolerance',1e-5,'MaxIterations',10000);
+    en(:,i)=sort(eigo(1:nv));
+end
+re=en;
+rev=vzlist;
+fn_mu=strcat('m',num2str(mu));
+fn_Delta=strcat('D',num2str(delta));
+fn_alpha=strcat('a',num2str(alpha));
+fn_wl=strcat('L',num2str(dim));
+fn_smoothpot=num2str(smoothpot);
+fn_mumax=strcat('mx',num2str(mumax));
+if (strcmp(smoothpot,'sin2')||strcmp(smoothpot,'cos'))
+    fn_peakpos=strcat('pk',num2str(peakpos));
+else
+    fn_peakpos='';
+end
+fn=strcat(fn_mu,fn_Delta,fn_alpha,fn_wl,fn_smoothpot,fn_mumax,fn_peakpos);
+save(strcat(fn,'.dat'),'re','-ascii');
+plot(vzlist,en)
+xlabel('V_Z(meV)')
+ylabel('V_{bias}(meV)')
+axis([0,4,-.3,.3])
+saveas(gcf,strcat(fn,'.png'))
+return 
