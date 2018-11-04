563c5222a39041805e59cdb0141aad4c37109a7f
Haining Pan <jack_pan1994@126.com>
2018-11-03 22:58:50 -0400

    Update LDOS_Vz.m

--- LDOS_Vz.m
+++ LDOS_Vz.m
@@ -1,10 +1,10 @@
 function [rev,re]=LDOS_Vz(mu,dim,smoothpot,mumax,peakpos)
 delta=0.2;
 alpha=5;
-vzlist=0:0.001:0.4;
+vzlist=0:0.01:0.4;
 omegalist=-0.3:0.001:0.3;
 en=zeros(length(vzlist),length(omegalist));
-for i=1:length(vzlist)    
+parfor i=1:length(vzlist)    
     vz=vzlist(i);
     nn=zeros(1,length(omegalist));
     for j=1:length(omegalist)
@@ -14,7 +14,6 @@ for i=1:length(vzlist)
     en(i,:)=nn;
 end
 re=en;
-surf(vzlist,omegalist,en','edgecolor','none');colorbar;view(2)
 rev=vzlist;
 fn_mu=strcat('m',num2str(mu));
 fn_Delta=strcat('D',num2str(delta));
@@ -29,10 +28,8 @@ else
     fn_peakpos='';
 end
 fn=strcat(fn_mu,fn_Delta,fn_alpha,fn_wl,fn_smoothpot,fn_mumax,fn_peakpos,fn_pos);
-save(strcat(fn,'.dat'),'re','-ascii');
-plot(vzlist,en)
-xlabel('V_Z(meV)')
-ylabel('V_{bias}(meV)')
-axis([0,4,-.3,.3])
-saveas(gcf,strcat(fn,'.png'))
+save(strcat('LDOS',fn,'.dat'),'re','-ascii');
+surf(vzlist,omegalist,en','edgecolor','none');colorbar;view(2);
+
+saveas(gcf,strcat('LDOS',fn,'.png'))
 end
