d12a9997bc53827be62b4cc981c26f87f95762da
Haining Pan <jack_pan1994@126.com>
2018-11-21 18:50:41 -0500

    Update aveDOS.m

--- aveDOS.m
+++ aveDOS.m
@@ -1,24 +1,16 @@
 vzlist=[0.1,0.2,0.3,0.4];
-for ii=1:4
+disorderlist=[0,0.1,0.5,1,2,3,5,10,20,30];
+for ii=1:length(vzlist)
     vz=vzlist(ii);
-dosstore=zeros(100,100);
-for i=1:100
-    dosstore(i,:)=DOS_Vzcut(.2,300,2,vz);
-end
-plot(linspace(-0.3,0.3,100),mean(dosstore));
-saveas(gcf,strcat(num2str(vz),'V0.1.png'));
-
-dosstore2=zeros(100,100);
-for i=1:100
-    dosstore2(i,:)=DOS_Vzcut(.2,300,5,vz);
-end
-plot(linspace(-0.3,0.3,100),mean(dosstore2));
-saveas(gcf,strcat(num2str(vz),'V0.5.png'));
-
-dosstore3=zeros(100,100);
-for i=1:100
-    dosstore3(i,:)=DOS_Vzcut(.2,300,10,vz);
-end
-plot(linspace(-0.3,0.3,100),mean(dosstore3));
-saveas(gcf,strcat(num2str(vz),'V1.png'));
+    dosstore=zeros(100,100);
+    for jj=1:length(disorderlist)
+        disorder=disorderlist(jj);
+        for i=1:100
+            dosstore(i,:)=DOS_Vzcut(.2,300,disorder,vz);
+        end
+        plot(linspace(-0.3,0.3,100),mean(dosstore));
+        xlabel('E(meV)');
+        ylabel('DOS');
+        saveas(gcf,strcat(num2str(vz),'V',num2str(disorder),'.png'));
+    end
 end
