bbada8dc58cd93ecb84aa25c736254bd5d341548
Haining Pan <jack_pan1994@126.com>
2018-11-23 13:38:09 -0500

    Update aveDOS.m

--- aveDOS.m
+++ aveDOS.m
@@ -2,10 +2,10 @@ vzlist=[0.1,0.2,0.3,0.4];
 disorderlist=[0,0.1,0.5,1,2,3,5,10,20,30];
 for ii=1:length(vzlist)
     vz=vzlist(ii);
-    dosstore=zeros(100,100);
+    dosstore=zeros(1000,100);
     for jj=1:length(disorderlist)
         disorder=disorderlist(jj);
-        for i=1:100
+        for i=1:1000
             dosstore(i,:)=DOS_Vzcut(.2,300,disorder,vz);
         end
         plot(linspace(-0.3,0.3,100),mean(dosstore));
