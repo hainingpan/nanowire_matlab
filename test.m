005fe71cf93ea9161e459a6c20b86763aa93e306
Haining Pan <jack_pan1994@126.com>
2018-11-26 00:16:03 -0500

    spectrum disorder

--- test.m
+++ test.m
@@ -1,3 +1,2 @@
-[L,U,p] = lu(A,'vector');
-Afun = @(x) U\(L\(x(p)));
-d = eigs(Afun,100,6,'smallestabs')
+vimplist=load("D:\\Nanowire\\bothlead\\Rp8\\m0.2D0.2a5L300Lv1.0pot.dat",'-ascii');
+spec_dis(1,.2,300,1,vimplist);
