37b8ee1f441b4c20e2c409625307c7a1c67a5e3d
Haining Pan <jack_pan1994@126.com>
2018-11-29 12:32:52 -0500

    Update spec_dis.m

--- spec_dis.m
+++ spec_dis.m
@@ -1,13 +1,13 @@
 %%spectrum for disorder
-function [rev,re]=spec_dis(a,mu,dim,v,vimp)
+function [rev,re,vimp]=spec_dis(a,mu,dim,v,vimp)
 % a=1;
 delta=0.2;
 alpha=5;
-vzlist=0:0.01:1.024;
+vzlist=0:0.01:2.048;
 nv=80;
 en=zeros(nv,length(vzlist));
 % pos=randperm(dim,numdis);
-% vimp=v*randn(dim,1);
+vimp=v*randn(dim,1);
 parfor i=1:length(vzlist)
     vz=vzlist(i);
     ham=hdis(a,mu,delta,vz,alpha,dim,vimp);
@@ -28,5 +28,6 @@ plot(vzlist,en)
 xlabel('V_Z(meV)')
 ylabel('V_{bias}(meV)')
 axis([0,vzlist(end),-.3,.3])
+line([sqrt(mu^2+delta^2),sqrt(mu^2+delta^2)],[-0.3,0.3])
 saveas(gcf,strcat(fn,'.png'))
 end
