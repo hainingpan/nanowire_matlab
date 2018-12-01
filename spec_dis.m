1cecd5f17010cdd416a570c4bdf5a5c676b45f54
Haining Pan <jack_pan1994@126.com>
2018-11-30 21:09:28 -0500

    Update spec_dis.m

--- spec_dis.m
+++ spec_dis.m
@@ -3,11 +3,13 @@ function [rev,re,vimp]=spec_dis(a,mu,dim,v,vimp)
 % a=1;
 delta=0.2;
 alpha=5;
-vzlist=0:0.01:2.048;
+vzlist=linspace(0,2.048*1.5,100);
 nv=80;
 en=zeros(nv,length(vzlist));
 % pos=randperm(dim,numdis);
-% vimp=v*randn(dim,1);
+if vimp==0
+    vimp=v*randn(dim,1);
+end
 parfor i=1:length(vzlist)
     vz=vzlist(i);
     ham=hdis(a,mu,delta,vz,alpha,dim,vimp);
