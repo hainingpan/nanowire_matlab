89c1ef2922c02e25853320fefd22b2a3685b5cac
Haining Pan <jack_pan1994@126.com>
2018-12-25 11:49:14 -0500

    quantum dot

---
+++ hqd.m
@@ -0,0 +1,19 @@
+%% Quantum dots (gaussian)
+function re=hqd(a,mu,delta,vz,alpha_R,mumax,l0,dim)
+t=25/a^2;
+sx=[0,1;1,0];
+sy=[0,-1i;1i,0];
+sz=[1,0;0,-1];
+alpha=alpha_R/(2*a);
+band11sm=(spdiags([ones(dim,1) ones(dim,1)],[-1,1],dim,dim));
+band1m1sm=(spdiags([ones(dim,1) -ones(dim,1)],[-1,1],dim,dim));
+deltalist=[zeros(l0,1);delta*ones(dim-l0,1)];
+diagdelta=spdiags(deltalist,0,dim,dim);
+eyesm=speye(dim);
+% mulist=mu*ones(dim,1);
+x=(1:dim)';
+mulist=mu-mumax*exp(-x.*x/(l0*l0));
+diagmulist=spdiags(mulist,0,dim,dim);
+re=kron(sz,(kron(eye(2),-t*band11sm+(2*t)*eyesm-diagmulist)+kron(sy,1i*alpha*band1m1sm)))+kron(eye(2),kron(sz,vz*eyesm))+kron(sx,kron(eye(2),diagdelta));
+end
+
---
+++ spec_qd.m
@@ -0,0 +1,33 @@
+%%spectrum for quantum dot
+function [rev,re]=spec_qd(a,mu,dim,mumax,l0)
+% a=1;
+delta=0.2;
+alpha=5;
+vzlist=linspace(0,2.048,100);
+nv=80;
+en=zeros(nv,length(vzlist));
+
+parfor i=1:length(vzlist)
+    vz=vzlist(i);
+    ham=hqd(a,mu,delta,vz,alpha,mumax,l0,dim);
+    eigo=eigs(ham,nv,0,'Tolerance',1e-5,'MaxIterations',20000);
+    en(:,i)=sort(eigo(1:nv));
+end
+re=en;
+rev=vzlist;
+fn_mu=strcat('m',num2str(mu));
+fn_Delta=strcat('D',num2str(delta));
+fn_alpha=strcat('a',num2str(alpha));
+fn_wl=strcat('L',num2str(dim));
+fn_mumax=strcat('mumax',num2str(mumax));
+fn_l0=strcat('l',num2str(l0));
+% fn_num=strcat('N',num2str(numdis));
+fn=strcat(fn_mu,fn_Delta,fn_alpha,fn_wl,fn_mumax,fn_l0);
+save(strcat(fn,'.dat'),'re','-ascii');
+plot(vzlist,en)
+xlabel('V_Z(meV)')
+ylabel('V_{bias}(meV)')
+axis([0,vzlist(end),-.3,.3])
+line([sqrt(mu^2+delta^2),sqrt(mu^2+delta^2)],[-0.3,0.3])
+saveas(gcf,strcat(fn,'.png'))
+end
