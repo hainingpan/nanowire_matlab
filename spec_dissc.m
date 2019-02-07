7c0f7899f07bcdf17de1164098c6f8697d07dc9f
Haining Pan <jack_pan1994@126.com>
2019-02-07 15:10:33 -0500

    add (disorder + sc gap collaping) spec and ham

--- spec_dissc.m
+++ spec_dissc.m
@@ -1,14 +1,18 @@
-%%for single band 
-function [rev,re]=spec_inhom(a,mu,dim,smoothpot,mumax,peakpos,sigma)
+%%spectrum for disorder & collapsing superconducting gap (deltalist=delta_0*sqrt(1-(VZ/Vc)^2))
+function [rev,re,vimp]=spec_dissc(a,mu,dim,v,vimp,vc)
 % a=1;
 delta=0.2;
-alpha=0.1;
-vzlist=0:0.01:1.024*0.5;
-nv=60;
+alpha=5;
+vzlist=linspace(0,2.048/2,100);
+nv=80;
 en=zeros(nv,length(vzlist));
+% pos=randperm(dim,numdis);
+if vimp==0
+    vimp=v*randn(dim,1);
+end
 for i=1:length(vzlist)
     vz=vzlist(i);
-    ham=hmu(a,mu,delta,vz,alpha,dim,smoothpot,mumax,peakpos,sigma);
+    ham=hdissc(a,mu,delta,vz,alpha,dim,vimp,vc);
     eigo=eigs(ham,nv,0,'Tolerance',1e-5,'MaxIterations',20000);
     en(:,i)=sort(eigo(1:nv));
 end
@@ -18,15 +22,9 @@ fn_mu=strcat('m',num2str(mu));
 fn_Delta=strcat('D',num2str(delta));
 fn_alpha=strcat('a',num2str(alpha));
 fn_wl=strcat('L',num2str(dim));
-fn_smoothpot=num2str(smoothpot);
-fn_mumax=strcat('mx',num2str(mumax));
-fn_sigma=strcat('sg',num2str(sigma));
-if (strcmp(smoothpot,'lorentz')||strcmp(smoothpot,'lorentzsigmoid'))
-    fn_peakpos=strcat('pk',num2str(peakpos));
-else
-    fn_peakpos='';
-end
-fn=strcat(fn_mu,fn_Delta,fn_alpha,fn_wl,fn_smoothpot,fn_mumax,fn_peakpos,fn_sigma);
+% fn_num=strcat('N',num2str(numdis));
+fn_v=strcat('v',num2str(v));
+fn=strcat(fn_mu,fn_Delta,fn_alpha,fn_wl,fn_v);
 save(strcat(fn,'.dat'),'re','-ascii');
 plot(vzlist,en)
 xlabel('V_Z(meV)')
