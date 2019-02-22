60123387e3cdfd284d623139c97c4a0e680cedb1
Haining Pan <jack_pan1994@126.com>
2019-02-22 13:54:10 -0500

    Create dosseinhom.m

---
+++ dosseinhom.m
@@ -0,0 +1,7 @@
+function re=dosseinhom(mu,Delta,vz,alpha_R,gamma,vc,dim,smoothpot,mumax,peakpos,sigma,omega,delta)
+a=1;
+[~,ham]=hsemu(a,mu,Delta,vz,alpha_R,gamma,vc,omega,1,0,dim,smoothpot,mumax,peakpos,sigma);
+K=(omega+1i*delta)*speye(4*dim)-ham;
+Kv=eigs(K,40,0,'Tolerance',1e-5,'MaxIterations',20000);
+re=-sum(imag(1./Kv))/pi;
+end
