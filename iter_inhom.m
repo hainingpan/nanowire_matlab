2beda1a55d14138ae4f78f2bbc4730f24dd92571
Haining Pan <jack_pan1994@126.com>
2019-02-22 14:00:59 -0500

    add initial value for iter_inhom

--- iter_inhom.m
+++ iter_inhom.m
@@ -1,5 +1,5 @@
-function re=iter_inhom(a,mu,delta,vz,alpha_R,gamma,vc,n,dim,smoothpot,mumax,peakpos,sigma)
-omega=0;
+function re=iter_inhom(a,mu,delta,vz,alpha_R,gamma,vc,n,dim,smoothpot,mumax,peakpos,sigma,omega)
+% omega=0;
 omega_n=hsemu(a,mu,delta,vz,alpha_R,gamma,vc,omega,n,0,dim,smoothpot,mumax,peakpos,sigma);
 % k=(omega_n-0)/(0-delta);
 % beta=-k;
