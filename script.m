781e1cbaf84ef62b631e22df930054aa083c8a4e
Haining Pan <jack_pan1994@126.com>
2019-04-06 12:56:08 -0400

    temp script

---
+++ script.m
@@ -0,0 +1,9 @@
+% vimp=load('D:\CMTC\bothlead\Rp16\ugly\1\vimp.txt');
+% [dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,1.2,300,1,vimp);
+% [dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,3,300,1,vimp);
+vimp=load('D:\CMTC\bothlead\Rp16\ugly\2\vimp.txt');
+[dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,1.2,300,1,vimp);
+[dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,3,300,1,vimp);
+% vimp=load('D:\CMTC\bothlead\Rp16\ugly\3\vimp.txt');
+% [dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,1.2,300,1,vimp);
+% [dosmap,rev,vimp]=spec_sedis_sp(1,1,.2,5,.2,3,300,1,vimp);
---
+++ script2.m
@@ -0,0 +1,8 @@
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,1,300,'exp',-1.2,0,40);
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,inf,300,'exp',-1.2,0,40);
+
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,1,300,'exp',-1.4,0,20);
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,inf,300,'exp',-1.4,0,20);
+
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,1,300,'exp',-1.1,0,50);
+[dosmap,rev]=spec_seinhom_sp(1,1,.2,5,.2,inf,300,'exp',-1.1,0,50);
