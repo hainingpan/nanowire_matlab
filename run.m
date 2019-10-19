2ca5cef231327f0c0e4c5b8122641ab2bfe9029e
Haining Pan <jackpan1994@gmail.com>
2019-10-19 15:06:36 -0400

    Update run.m

--- run.m
+++ run.m
@@ -5,19 +5,19 @@
 % spec_segap_sp(1,1,.2,5,.2,3,300,.06,randlist);
 
 
-spec_seqd_sp(1,1,.2,5,.2,1,1.7,20,100);
-spec_seqd_sp(1,1,.2,5,.2,1,0.6,40,300);
-spec_seinhom_sp(1,1,.2,5,.2,1,1.4,15,100);
-spec_seinhom_sp(1,1,.2,5,.2,1,1.2,40,300);
-randlist=load('d:\cmtc\bothlead\Rp17\ugly\1\randlist.dat');
-spec_sedis_sp(1,1,.2,5,.2,1.2,100,1,randlist);
-randlist=load('d:\cmtc\bothlead\Rp17\ugly\3\randlist.dat');
-spec_sedis_sp(1,1,.2,5,.2,1.2,100,1,randlist);
-randlist=load('d:\cmtc\bothlead\Rp16\ugly\1\vimp.txt');
-spec_sedis_sp(1,1,.2,5,.2,1.2,300,1,randlist);
-randlist=load('d:\cmtc\bothlead\Rp19\gvar1\104\randlist.dat');
-spec_seg_sp(1,1,.2,5,.2,1.2,100,0.8,randlist);
+% spec_seqd_sp(1,1,.2,5,.2,1,1.7,20,100);
+% spec_seqd_sp(1,1,.2,5,.2,1,0.6,40,300);
+% spec_seinhom_sp(1,1,.2,5,.2,1,100,'exp',-1.4,0,15);
+% spec_seinhom_sp(1,1,.2,5,.2,1,300,'exp',-1.2,0,40);
+% randlist=load('d:\cmtc\bothlead\Rp17\ugly\1\randlist.dat');
+% spec_sedis_sp(1,1,.2,5,.2,1.2,100,1,randlist);
+% randlist=load('d:\cmtc\bothlead\Rp17\ugly\3\randlist.dat');
+% spec_sedis_sp(1,1,.2,5,.2,1.2,100,1,randlist);
+% randlist=load('d:\cmtc\bothlead\Rp16\ugly\1\vimp.txt');
+% spec_sedis_sp(1,1,.2,5,.2,1.2,300,1,randlist);
+% randlist=load('d:\cmtc\bothlead\Rp19\gvar1\104\randlist.dat');
+% spec_seg_sp(1,1,.2,5,.2,1.2,100,0.8,randlist);
 randlist=load('d:\cmtc\bothlead\Rp19\gvar1\115\randlist.dat');
 spec_seg_sp(1,1,.2,5,.2,1.2,100,0.8,randlist);
-randlist=load('d:\cmtc\bothlead\Rp19\gvar3\13\vimp.txt');
-spec_seg_sp(1,1,.2,5,.2,1.2,300,0.6,randlist);
+% randlist=load('d:\cmtc\bothlead\Rp19\gvar3\13\vimp.txt');
+% spec_seg_sp(1,1,.2,5,.2,1.2,300,0.6,randlist);
