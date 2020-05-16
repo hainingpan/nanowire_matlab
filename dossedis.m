3a7243706053d3163d9bd951dc968fffde76c92b
Haining Pan <jack_pan1994@126.com>
2020-05-15 23:10:34 -0400

    add periodic boundary condition

--- dossedis.m
+++ dossedis.m
@@ -1,6 +1,6 @@
 function re=dossedis(a,mu,Delta,vz,alpha_R,gamma,vc,dim,vimp,omega,delta)
 % a=1;
-[~,ham]=hsedis(a,mu,Delta,vz,alpha_R,gamma,vc,omega,1,0,dim,vimp);
+[~,ham]=hsedis_period(a,mu,Delta,vz,alpha_R,gamma,vc,omega,1,0,dim,vimp);
 K=(omega+1i*delta)*speye(4*dim)-ham;
 Kv=eigs(K,40,0,'Tolerance',1e-5,'MaxIterations',20000);
 re=-sum(imag(1./Kv))/pi;
